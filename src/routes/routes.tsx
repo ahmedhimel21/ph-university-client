c112d31e818cec83fc142f267d69b54f05b83a8d
ahmedhimel21 <ahmedhimel000@gmail.com>
2024-07-04 01:34:59 +0600

    refactor faculty and student routes

--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -7,6 +7,8 @@ import { routeGenerator } from "../utils/routeGenerator";
 
 import { facultyPaths } from "./faculty.routes";
 import { studentPaths } from "./student.routes";
+import Login from "../pages/auth/Login";
+import Register from "../pages/auth/Register";
 
 export const router = createBrowserRouter([
   {
@@ -33,5 +35,13 @@ export const router = createBrowserRouter([
     path: "/student",
     element: <App></App>,
     children: routeGenerator(studentPaths),
+  },
+  {
+    path: "/login",
+    element: <Login></Login>,
+  },
+  {
+    path: "/register",
+    element: <Register></Register>,
   },
 ]);
