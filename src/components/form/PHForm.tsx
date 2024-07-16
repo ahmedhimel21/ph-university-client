0c4596ca975c16511fa7cc7c739adbccf77797c1
ahmedhimel21 <ahmedhimel000@gmail.com>
2024-07-16 16:43:59 +0600

    handle form validation using zod, hook-form-resolver

--- src/components/form/PHForm.tsx
+++ src/components/form/PHForm.tsx
@@ -3,13 +3,30 @@ import { Form } from "antd";
 import { ReactNode } from "react";
 import { FormProvider, SubmitHandler, useForm } from "react-hook-form";
 
+type TFormConfig = {
+  defaultValues?: Record<string, unknown>;
+  resolver?: any;
+};
+
 type TFormProps = {
   children: ReactNode;
   onSubmit: SubmitHandler<any>;
-};
+} & TFormConfig;
 
-const PHForm = ({ children, onSubmit }: TFormProps) => {
-  const methods = useForm();
+const PHForm = ({
+  children,
+  onSubmit,
+  resolver,
+  defaultValues,
+}: TFormProps) => {
+  const formConfig: TFormConfig = {};
+  if (defaultValues) {
+    formConfig["defaultValues"] = defaultValues;
+  }
+  if (resolver) {
+    formConfig["resolver"] = resolver;
+  }
+  const methods = useForm(formConfig);
   return (
     <FormProvider {...methods}>
       <Form layout="vertical" onFinish={methods.handleSubmit(onSubmit)}>
--- src/pages/admin/academicSemester/CreateAcademicSemester.tsx
+++ src/pages/admin/academicSemester/CreateAcademicSemester.tsx
@@ -4,6 +4,8 @@ import { Button, Col, Flex } from "antd";
 import PHSelect from "../../../components/form/PHSelect";
 import { semesterOptions } from "../../../constants/Semester";
 import { monthOptions } from "../../../constants/global";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { academicSemesterSchema } from "../../../schemas/academicManagement.schema";
 
 const currentYear = new Date().getFullYear();
 const yearOptions = [0, 1, 2, 3, 4].map((number) => ({
@@ -23,10 +25,14 @@ const CreateAcademicSemester = () => {
     };
     console.log(semesterData);
   };
+
   return (
     <Flex justify="center" align="center">
       <Col span={6}>
-        <PHForm onSubmit={onSubmit}>
+        <PHForm
+          onSubmit={onSubmit}
+          resolver={zodResolver(academicSemesterSchema)}
+        >
           <PHSelect
             label="Name"
             name="name"
